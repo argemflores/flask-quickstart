f430eb832d4cb0526708eca14ebedc7ddeb3265e
Argem Flores <argemflores@gmail.com>
2022-10-06 20:47:15 +0800

    Try HTML escaping

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-"""Flask"""
+"""Modules"""
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -7,3 +8,8 @@ app = Flask(__name__)
 def hello_world():
     """Hello world"""
     return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def hello(name):
+    """Hello name"""
+    return f"Hello, {escape(name)}!"
