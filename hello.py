f2f702d1929c1ea9afaa69379ef7fb91363e2b10
Argem Flores <argemflores@gmail.com>
2022-10-06 20:57:31 +0800

    Try routing

--- hello.py
+++ hello.py
@@ -4,12 +4,22 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    """Hello world"""
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     """Hello world"""
+#     return "<p>Hello, World!</p>"
 
 @app.route("/<name>")
-def hello(name):
+def hello_name(name):
     """Hello name"""
     return f"Hello, {escape(name)}!"
+
+@app.route('/')
+def index():
+    """Index route"""
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    """Hello world route"""
+    return 'Hello, World'
