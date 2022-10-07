3567827b912b1e7a69b403c4d179119d1a1b3e4a
Argem Flores <argemflores@gmail.com>
2022-10-07 15:29:54 +0800

    Try request object

--- hello.py
+++ hello.py
@@ -52,25 +52,18 @@ def show_the_login_form():
     """Show the login form"""
     return 'show the login form'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    """Login using GET and POST"""
-    if request.method == 'POST':
-        return do_the_login()
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     """Login using GET and POST"""
+#     if request.method == 'POST':
+#         return do_the_login()
 
-    return show_the_login_form()
+#     return show_the_login_form()
 
 @app.route('/user/<username>')
 def profile(username):
     """User profile"""
     return f'{username}\'s profile'
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
-    print(url_for('static', filename='style.css'))
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -85,8 +78,58 @@ def hello(name=None):
     """
     return render_template('hello.html', name=name)
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    """Login
+
+    Returns:
+        None: Print login message
+    """
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+
+        error = 'Invalid username/password'
+    else:
+        error = 'View only'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    """Validate login
+
+    Args:
+        username (str): Username
+        password (str): Password
+
+    Returns:
+        bool: True if valid; False otherwise
+    """
+    return username and password
+
+def log_the_user_in(username):
+    """Show login message
+
+    Args:
+        username (str): Username
+
+    Returns:
+        None: Print login message
+    """
+    return f'{username} logged in!'
+
 with app.test_request_context('/hello', method='POST'):
     # now you can do something with the request until the
     # end of the with block, such as basic assertions:
     assert request.path == '/hello'
     assert request.method == 'POST'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+    print(url_for('static', filename='style.css'))
