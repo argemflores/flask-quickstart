6aea937a84810983bdaa22a4ab24fca4ceb6eac3
Argem Flores <argemflores@gmail.com>
2022-10-07 15:28:36 +0800

    Try context locals

--- hello.py
+++ hello.py
@@ -84,3 +84,9 @@ def hello(name=None):
         None: Print hello.html contents
     """
     return render_template('hello.html', name=name)
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
